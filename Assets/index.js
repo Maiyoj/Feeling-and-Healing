d7f47c785725fec4563535badd3796495143fac7
Maiyoj <valentinemaiyo1995@gmail.com>
2022-06-21 07:54:42 +0300

    styled therapist images

---
+++ Assets/index.js
@@ -0,0 +1,19 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'e746e9526fmshfd7b8fb75fdde2cp19cfb4jsn5e4f37b754cc',
+            'X-RapidAPI-Host': 'mental-health-info-api.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://mental-health-info-api.p.rapidapi.com/news/thetimes', options)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err));
+
+})
+
+
+
