3c2bb59a65873ae896cc2388ecb4b8d6b2b30c15
Maiyoj <valentinemaiyo1995@gmail.com>
2022-06-21 22:05:02 +0300

    add public url to fetch data

--- Assets/index.js
+++ Assets/index.js
@@ -11,6 +11,75 @@ document.addEventListener('DOMContentLoaded', function(){
     fetch('https://mental-health-info-api.p.rapidapi.com/news/thetimes', options)
         .then(response => response.json())
         .then(data => {
+            console.log(data)
+        })
+        .catch(err => console.error(err));
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*document.addEventListener('DOMContentLoaded', function(){
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'e746e9526fmshfd7b8fb75fdde2cp19cfb4jsn5e4f37b754cc',
+            'X-RapidAPI-Host': 'mental-health-info-api.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://mental-health-info-api.p.rapidapi.com/news/thetimes', options)
+        .then(response => response.json())
+        .then(data => {
             // console.log(data[0]?.title)
         let paragraph = data[0]?.title
         let addp = document.getElementById('tile blog')
@@ -19,9 +88,7 @@ document.addEventListener('DOMContentLoaded', function(){
         })
         .catch(err => console.error(err));
 
-        
-
 })
-
+*/
 
 
